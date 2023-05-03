d403690bfb773d5ae01025b87af2b51952ba89fa
AnakinSkywalker066 <Leegovols@gmail.com>
2023-05-03 01:01:10 -0400

    Add project files.

---
+++ BabyYodaReskin.cs
@@ -0,0 +1,38 @@
+﻿using MelonLoader;
+using BTD_Mod_Helper;
+using BabyYoda;
+using Il2CppAssets.Scripts.Models;
+using BTD_Mod_Helper.Extensions;
+using BTD_Mod_Helper.Api.Display;
+using JetBrains.Annotations;
+using Octokit;
+using Il2CppAssets.Scripts.Models.TowerSets;
+using Il2CppAssets.Scripts.Models.Towers;
+
+[assembly: MelonInfo(typeof(Reskin), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace BabyYoda;
+
+public class Reskin : BloonsTD6Mod
+{
+    
+    public override void OnNewGameModel(GameModel gameModel)
+    {
+        foreach (var towerModel in gameModel.towers)
+        {
+            towerModel.displayScale = 50f;
+            
+            if (towerModel.baseId == "TimeMaster")
+            {
+                towerModel.ApplyDisplay<Yoda>();
+            }
+        }
+    }
+    public class Yoda : ModCustomDisplay
+    {
+        public override string AssetBundleName => "yoda";
+        public override string PrefabName => "BabyYoda";
+        
+    }
+}
---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+﻿namespace BabyYoda;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "36";
+    public const string Version = "1.0.0";
+    public const string Name = "TemplateMod";
+
+    public const string Description = "Reskin";
+
+    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+}
