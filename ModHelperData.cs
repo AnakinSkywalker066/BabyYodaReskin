7f72f6d435e81141dadeaacc22d41c8091952320
AnakinSkywalker066 <Leegovols@gmail.com>
2023-05-03 11:10:46 -0400

    update!

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -9,6 +9,6 @@ public static class ModHelperData
     public const string Description = "YOU MUST HAVE TIMEMASTER MOD FROM 1330 STUDIOS DISCORD SERVER! THIS DOES NOT GRANT ACCESS TO THE TIMEMASTER ITSELF!!!"
         +  "https://discord.gg/1330studios";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "AnankinSkywalker066"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "BabyYodaReskin"; // TODO add your repo name here, also in the download url in README.md
 }
