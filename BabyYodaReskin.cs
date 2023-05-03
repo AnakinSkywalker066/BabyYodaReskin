67f1193f75bafd1ecac044aefd05df09372767d7
AnakinSkywalker066 <Leegovols@gmail.com>
2023-05-03 02:36:13 -0400

    up

--- BabyYodaReskin.cs
+++ BabyYodaReskin.cs
@@ -4,13 +4,6 @@ using BabyYoda;
 using Il2CppAssets.Scripts.Models;
 using BTD_Mod_Helper.Extensions;
 using BTD_Mod_Helper.Api.Display;
-using JetBrains.Annotations;
-using Octokit;
-using Il2CppAssets.Scripts.Models.TowerSets;
-using Il2CppAssets.Scripts.Models.Towers;
-using UnityEngine;
-using Il2CppAssets.Scripts.Unity.Display;
-using BTD6E_Module_Helper;
 
 [assembly: MelonInfo(typeof(Reskin), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
@@ -22,8 +15,10 @@ public class Reskin : BloonsTD6Mod
     
     public override void OnNewGameModel(GameModel gameModel)
     {
+        
         foreach (var towerModel in gameModel.towers)
         {
+            
             towerModel.displayScale = 50f;
             
             if (towerModel.baseId == "TimeMaster")
