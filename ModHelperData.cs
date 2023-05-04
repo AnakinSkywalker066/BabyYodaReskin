e380cb69c4b470447080e3856833460fbcb91a3b
AnakinSkywalker066 <Leegovols@gmail.com>
2023-05-04 11:37:51 -0400

    update

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -5,9 +5,8 @@ public static class ModHelperData
     public const string WorksOnVersion = "36";
     public const string Version = "1";
     public const string Name = "Baby Yoda";
-
-    public const string Description = "In Honor Of MAY THE 4TH I MADE BABY YODA IN BTD6! " + "YOU MUST HAVE TIMEMASTER MOD FROM 1330 STUDIOS DISCORD SERVER! THIS DOES NOT GRANT ACCESS TO THE TIMEMASTER ITSELF!!! "
-        +  " https://discord.gg/1330studios" + " Happy May The 4th!";
+    public const string Icon = "Icon.png";
+    public const string Description = "In Honor Of MAY THE 4TH I MADE BABY YODA IN BTD6! " + "YOU MUST HAVE TIMEMASTER MOD FROM 1330 STUDIOS DISCORD SERVER! THIS DOES NOT GRANT ACCESS TO THE TIMEMASTER ITSELF!!! " + " https://discord.gg/1330studios" + " Happy May The 4th!";
 
     public const string RepoOwner = "AnankinSkywalker066"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "BabyYodaReskin"; // TODO add your repo name here, also in the download url in README.md
